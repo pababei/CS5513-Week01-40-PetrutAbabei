53b6a43c76b547f8575fcc05b17bdae0042e9cde
PetrutAbabei <2091799-PetrutAbabei@users.noreply.replit.com>
2023-08-19 10:41:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let http = require('http');
+let server = http.createServer(
+  function(req, res){
+    let text;
+    
+    if(req.url == "/cats"){
+      text = "Here are all the cats...";
+    }
+    else if (req.url == "/dogs"){
+      text = "There are some dogs as well...";
+    }
+
+    res.writeHead(200, {"Content-Type": "text/plain"});
+
+    res.end(text);
+  }
+)
+
+server.listen(8080, "0.0.0.0");
